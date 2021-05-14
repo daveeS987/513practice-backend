873bcd00eb5d2d6b0dc002f04c82dcd4756acd07
Davee Sok <daveesok@gmail.com>
2021-05-13 19:27:03 -0500

    initializing

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import pusher from 'pusher';
+
+// app config
+
+// middlewares
+
+// DB config
+
+// api routes
+
+// listen
